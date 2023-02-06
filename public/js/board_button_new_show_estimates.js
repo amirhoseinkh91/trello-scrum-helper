ed2821a9e00fbe59be2cfdb9d204cf3f50a4e937
AmirHosein <amir.hossein.khalouei@gmail.com>
2023-02-06 08:32:32 +0330

    add actions to see what appears on model

--- public/js/board_button_new_show_estimates.js
+++ public/js/board_button_new_show_estimates.js
@@ -1,8 +1,28 @@
 const newShowEstimates = async function (trello, opts) {
   return trello.modal({
     title: "Estimates",
-    url: './../EstimatesModal.html',
-    fullscreen: true
+    url: './../../public/EstimatesModal.html',
+    fullscreen: true,
+    actions: [{
+      icon: './../../public/icons/wall-clock-24.png',
+      url: 'https://google.com',
+      alt: 'Leftmost',
+      position: 'left',
+    }, {
+      icon: './../../public/icons/wall-clock-24.png',
+      callback: (tr) => tr.popup({
+        title: tr.localizeKey('appear_in_settings'),
+        url: 'settings.html',
+        height: 164,
+      }),
+      alt: 'Second from left',
+      position: 'left',
+    }, {
+      icon: './../../public/icons/wall-clock-24.png',
+      callback: () => console.log(':tada:'),
+      alt: 'Right side',
+      position: 'right',
+    }],
   });
 };
 
